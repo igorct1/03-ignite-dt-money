f8c6d976fed6b597c74dd08aedc013103ddff2fb
igoreason <igorct77@gmail.com>
2023-07-15 18:17:34 -0300

    feat: table transactions

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,11 +1,40 @@
 import { Header } from '../../components/Header'
 import { Summary } from '../../components/Summary'
+import {
+  PriceHighlight,
+  TransactionsContainer,
+  TransactionsTable,
+} from './styles'
 
 export function Transactions() {
   return (
     <div>
       <Header />
       <Summary />
+
+      <TransactionsContainer>
+        <TransactionsTable>
+          <tbody>
+            <tr>
+              <td width="50%">Desenvolvimento de site</td>
+              <td>
+                <PriceHighlight variant="income">R$ 12.000,00</PriceHighlight>
+              </td>
+              <td>Venda</td>
+              <td>12/04/2022</td>
+            </tr>
+
+            <tr>
+              <td width="50%">Laptop</td>
+              <td>
+                <PriceHighlight variant="outcome">- R$ 2.000,00</PriceHighlight>
+              </td>
+              <td>Pessoal</td>
+              <td>12/04/2022</td>
+            </tr>
+          </tbody>
+        </TransactionsTable>
+      </TransactionsContainer>
     </div>
   )
 }
